0cdfd6a26e20d4c5a85753d7a263a88a7ecbc62d
llyllr <79972938+llyllr@users.noreply.github.com>
2022-08-17 09:07:09 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,12 +2,15 @@ import os, time,re, asyncio
 import discord
 from discord.ext import commands, tasks
 intents = discord.Intents().all()
-clientDC = commands.Bot(command_prefix='*',intents=intents)
+clientDC = commands.Bot(intents=intents)
 clientDC.remove_command('help')
 @clientDC.event
 async def on_ready():
   guild=clientDC.get_guild(1004238305094811769)
   channel=guild.get_channel(1004238305094811772)
-  await channel.send(content=i)
+  i=0
+  while True:
+    await channel.send(content=i)
+    i+=1
 
 clientDC.run('MTAwNzQ5ODA2MDU1MDYzNTU3MA.GB40C9.UaxN-iX5rt8JOivchwoQfIXVm7nxQl0x-ymuPo')
