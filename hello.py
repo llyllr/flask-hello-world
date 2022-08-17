5e7c6f76892a36262afa77b1917128f09a6ef430
llyllr <79972938+llyllr@users.noreply.github.com>
2022-08-17 08:51:21 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+import os, time,re, asyncio
+import discord
+from discord.ext import commands, tasks
+intents = discord.Intents().all()
+clientDC = commands.Bot(command_prefix='*)',intents=intents)
+clientDC.remove_command('help')
+@clientDC.event
+async def on_ready():
+  guild=clientDC.get_guild(1004238305094811769)
+  channel=guild.get_channel(1004238305094811772)
+  i=0
+  while True:
+    await channel.send(content=i)
+    i+=1
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
-
+clientDC.run('MTAwNzQ5ODA2MDU1MDYzNTU3MA.GB40C9.UaxN-iX5rt8JOivchwoQfIXVm7nxQl0x-ymuPo')
